323fd6f0b30545949da835a57faf5214c2c28421
Galliss13 <matheusgalindo1313@gmail.com>
2022-12-13 17:39:56 -0300

    feat: verifica se cpf ou phone são números customerSchemaValidation

--- src/middlewares/customers.middleware.js
+++ src/middlewares/customers.middleware.js
@@ -4,12 +4,17 @@ import { customerSchema } from "../schemas/customer.model.js";
 export async function customerSchemaValidation (req, res, next) {
 
     const customer = req.body;
-    const {phone, cpf} = req.body;
-
+    
     const { error } = customerSchema.validate(customer, {abortEarly: false})
     if (error) {
         const errors = error.details.map(detail => detail.message)
         return res.status(400).send(errors)
+    }
+    
+    const {phone, cpf} = req.body;
+    
+    if(parseInt(phone).isNaN() || parseInt(cpf).isNaN()) {
+        return res.sendStatus(400)
     }
 
     next()
